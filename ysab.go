1c0d67c01580c3c035cc30de9aae421461b44f61
yunsonbai <yunsonbai@163.com>
2023-05-05 16:57:39 +0800

    remove some code

--- ysab.go
+++ ysab.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"os"
-	"runtime/pprof"
-
 	"github.com/yunsonbai/ysab/worker"
 )
 
 func main() {
-	f, _ := os.OpenFile("cpu.prof", os.O_RDWR|os.O_CREATE, 0644)
-	pprof.StartCPUProfile(f)
+	// f, _ := os.OpenFile("cpu.prof", os.O_RDWR|os.O_CREATE, 0644)
+	// pprof.StartCPUProfile(f)
 	worker.StartWork()
-	defer pprof.StopCPUProfile()
+	// defer pprof.StopCPUProfile()
 }
